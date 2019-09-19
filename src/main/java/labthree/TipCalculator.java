10efc0d86af1ce9fb9a0ba310e723a0fa7b59aaf
jjumadin <jjumadin@aldenv64-6.local>
2019-09-18 22:59:06 -0400

    Starter files for lab 03 in CMPSC 100-01 Fall 2019

---
+++ src/main/java/labthree/TipCalculator.java
@@ -0,0 +1,37 @@
+package labthree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Date;
+import java.util.Scanner;
+
+public class TipCalculator {
+
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Janyl Jumadinova " + new Date());
+    // declare and initialize scanner
+    Scanner scanner = new Scanner(System.in);
+
+    // Step One: ask the user for, read in and display the person's name
+    System.out.println("Please enter your name");
+    String name = scanner.nextLine();
+    System.out.println(name + ", welcome to the tip calculator.");
+    
+    // TODO Step Two: ask the user for, read in and display the amount of the bill
+
+    // TODO Step Three: Ask the user for, read in and display the tip percentage
+
+    // TODO Step Four: Calculate and display the tip amount
+
+    // TODO Step Five: Calculate and display the total bill
+
+    // TODO Step Six: Ask the user for, read in and display the number of bill payers
+
+    // TODO Step Seven: Calculate and display the amount each person should pay
+
+    // TODO Step Eight: Display a final thankyou message
+
+  }
+
+}
